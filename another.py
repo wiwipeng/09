6bd3c66274c99b7f8587835adea33c981eab4612
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2021-12-23 21:12:54 +0800

    Update another.py

--- another.py
+++ another.py
@@ -9,7 +9,6 @@ a_count=0 # initial A count
 b_count=0 # initial B count
 for i in range(4):
     answer+=str(items[i])
-
 while(True):
     number=st.text_input('Enter the number: ')
     st.write(answer)
@@ -29,4 +28,4 @@ while(True):
         st.write('{0}A{1}B'.format(a_count,b_count))
         a_count=0
         b_count=0
-        pass
+        F
