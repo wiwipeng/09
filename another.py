21c4a071c1cc59ddd2ce3d687db6cb43514d2ea2
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2021-12-23 21:06:15 +0800

    Update another.py

--- another.py
+++ another.py
@@ -7,7 +7,7 @@ import streamlit as st
 items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 random.shuffle(items)
 #answer=''
-st.write(answer)
+#st.write(answer)
 a_count=0 # initial A count
 b_count=0 # initial B count
 #cofirm_input = st.button('輸入確認')
