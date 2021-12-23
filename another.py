159e1de1126d4c91553a510247efecf47d238e2a
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2021-12-23 20:36:53 +0800

    Update another.py

--- another.py
+++ another.py
@@ -5,7 +5,7 @@ import random
 import streamlit as st
 
 items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-random.shuffle(items)
+#random.shuffle(items)
 answer=''
 a_count=0 # initial A count
 b_count=0 # initial B count
@@ -13,7 +13,7 @@ b_count=0 # initial B count
 #if cofirm_input:
 for i in range(4):
     answer+=str(items[i])
-while number==answer:
+while(True):
     number=st.text_input('Enter the number: ')
     st.write(answer)
     if not number.isdigit(): 
