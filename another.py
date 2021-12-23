16f2bdc534f2f48617d7447024b5c23fef1cc896
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2021-12-23 20:42:47 +0800

    Update another.py

--- another.py
+++ another.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 #items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 #random.shuffle(items)
-answer = random.sample(range(1, 10), 4)
+items = random.range(1, 10)
 answer=''
 a_count=0 # initial A count
 b_count=0 # initial B count
